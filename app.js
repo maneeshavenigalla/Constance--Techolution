9eb199148a48888b7f6a1d70c9e09bced4e267eb
Maneesha Venigalla <maneesha@Maneeshas-MacBook-Pro.local>
2019-07-14 22:57:33 +0530

    ---

--- app.js
+++ app.js
@@ -568,34 +568,6 @@ bot
           )
         );
       });
-      // function getImages(uri) {
-      //   var request = require("request");
-      //   var url = require("url");
-      //   var cheerio = require("cheerio");
-      //   var path = require("path");
-      //   var fs = require("fs");
-
-      //   request(uri, function(error, response, body) {
-      //     if (!error && response.statusCode == 200) {
-      //       $ = cheerio.load(body);
-      //       imgs = $("img").toArray();
-      //       console.log("Downloading...");
-      //       imgs.forEach(function(img) {
-      //         //console.log(img.attribs.src)
-      //         process.stdout.write(".");
-      //         img_url = img.attribs.src;
-      //         if (/^https?:\/\//.test(img_url)) {
-      //           img_name = path.basename(img_url);
-      //           request(img_url).pipe(fs.createWriteStream(img_name));
-      //         }
-      //       });
-      //       console.log("Done!");
-      //     }
-      //   });
-      // }
-      // getImages(
-      //   "https://cstcor-cdn-endpoint.azureedge.net/assets//images/logos/constance_logo_rvb.png?v=0.1.2-beta"
-      // );
 
       // var download = function(uri, filename, callback) {
       //   request.head(uri, function(err, res, body) {
